c101d4e3e17a4fca9b5adc69fb3dfe46e28be147
DieterDoman <dieter.doman@gmail.com>
2019-12-05 08:00:18 +0200

    Completed some of the udemy lectures.

---
+++ src/lectures/Part1Basics/Functions.scala
@@ -0,0 +1,42 @@
+package lectures.Part1Basics
+
+object Functions extends App{
+  def afunction(a: String, b: Int): String = {
+    a + " " + b
+  }
+
+  def repeatedFunction(a: String, n: Int): String = {
+    if (n == 1)
+      a
+    else
+      a + repeatedFunction(a, n - 1)
+  }
+
+  def greetingFunction(name: String, age: Int): String = {
+    s"Hi my name is ${name} and my age is ${age}"
+  }
+
+  def factorial(n: Int): Int = {
+    if (n <= 0)
+      1
+    else
+      n * factorial(n - 1)
+  }
+
+  def fibonaci(n: Int): Int = {
+    if (n <= 2)
+      1
+    else
+      fibonaci(n - 1) + fibonaci(n - 2)
+  }
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t: Int): Boolean = {
+      if (t <= 1)
+        true
+      else
+      n % t != 0 && isPrimeUntil(t-1)
+    }
+    isPrimeUntil(n / 2)
+  }
+}
---
+++ src/lectures/Part1Basics/ValuesVariablesTypes.scala
@@ -0,0 +1,11 @@
+package lectures.Part1Basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42;
+  println(x)
+}
+
+
+object Expressions extends App {
+  val x = 42 + 5 / 2
+}
